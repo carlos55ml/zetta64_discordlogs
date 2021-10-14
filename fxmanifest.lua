7e4675375bb7b1805a7e4aa17389bee5fd70dc05
carlos55ml <cmoranlozano55@gmail.com>
2021-10-14 16:56:51 +0200

    [FIX] Config moved to server side.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,10 +6,11 @@ author 'zetta64'
 
 name 'zetta64_discordlogs'
 
-version '1.3'
+version '1.4'
 
 description 'Discord logs for FiveM servers using webhooks.'
 
-shared_script 'config.lua'
-
-server_script 'z64_dl_server_main.lua'
+server_script {
+    'z64_dl_server_main.lua',
+    'config.lua'
+}
